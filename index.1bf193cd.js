03628506ac1a432efa32602c1a348fe0548b231f
ZnSt <93055116+ZnSt@users.noreply.github.com>
2022-09-22 05:30:28 +0000

    Deploying to gh-pages from @ ZnSt/CRUD@1af96d95cc40fac2474e73d7ae0c228a559512f5 🚀

--- index.1bf193cd.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={method:"PATCH",headers:{"Content-Type":"application/json"},body:JSON.stringify({title:"Большая новая тестовая книга по HTML"})};fetch("".concat("http://localhost:7777","/books/13"),t).then((function(t){return t.json()})).then((function(t){return console.log(t)}))}();
-//# sourceMappingURL=index.1bf193cd.js.map
