ec7531396d2832e6751f1bc13bf92e3c7a53a389
ZnSt <93055116+ZnSt@users.noreply.github.com>
2022-09-23 04:31:42 +0000

    Deploying to gh-pages from @ ZnSt/CRUD@415aacfc125f581cf4bf5545ce3dca266e69785a 🚀

---
+++ index.bd5e8d68.js
@@ -0,0 +1,2 @@
+!function(){var n,o="http://localhost:7777";fetch("".concat(o,"/books")).then((function(n){return n.json()})).then((function(n){return console.log(n)})),n=1,fetch("".concat(o,"/books/").concat(n)).then((function(n){return n.json()})).then((function(n){return console.log(n)}))}();
+//# sourceMappingURL=index.bd5e8d68.js.map
---
+++ index.c85a2dec.js
@@ -0,0 +1,2 @@
+fetch("http://localhost:7777/books").then((o=>o.json())).then((o=>console.log(o))),fetch(`http://localhost:7777/books/${1}`).then((o=>o.json())).then((o=>console.log(o)));
+//# sourceMappingURL=index.c85a2dec.js.map
